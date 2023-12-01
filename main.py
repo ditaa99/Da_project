a8abe4b811238c9ade65f30efccc406e7127cdb7
Dita Pelaj <you@example.com>
2023-12-02 00:10:50 +0100

    added plots and explanations

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ Traffic Accident Analysis in Zurich
 # Import libraries
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
+
 
 # Specify the file path or URL
 file_path = 'RoadTrafficAccidentLocations.csv'
@@ -41,13 +43,8 @@ accidentDf['RoadType_en'] = accidentDf['RoadType_en'].astype('category')
 """checking for null values"""
 # print(accidentDf.isnull().sum())
 
-"""Plot the frequency of different types of accidents."""
-# Get the frequency of different types of accidents
-accident_type_freq = accidentDf['AccidentType_en'].value_counts()
 
-# Print the frequency
-print(accident_type_freq)
-
+""" Bar Plot for Accident Types with Labels"""
 # Create a dictionary for accident types
 accident_types = {
     "at0": "Skidding/self-accident",
@@ -72,8 +69,13 @@ accident_type_freq = accidentDf['AccidentType_en'].value_counts()
 # Create a new figure and set the figure size
 fig, ax = plt.subplots(figsize=(10, 6))
 
-# Plot the frequency of different types of accidents
-accident_type_freq.plot(kind='bar', ax=ax)
+# Plot the frequency of different types of accidents with labels
+bars = ax.bar(accident_type_freq.index, accident_type_freq.values)
+
+# Add labels to the bars
+for bar in bars:
+    yval = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width() / 2, yval + 20, round(yval, 1), ha='center', va='bottom')
 
 # Rotate the x-axis labels and make them fit into the figure
 fig.autofmt_xdate()
@@ -83,6 +85,7 @@ ax.set_title('Frequency of Different Types of Accidents')
 ax.set_xlabel('Accident Type')
 ax.set_ylabel('Frequency')
 
+# plt.savefig("./plots/accident_types.png")
 # Show the plot
 plt.show()
 
@@ -113,6 +116,7 @@ ax.set_title('Frequency of Accidents for Each Day of the Week')
 ax.set_xlabel('Day of the Week')
 ax.set_ylabel('Frequency')
 
+# plt.savefig("./plots/frequency_accidents.png")
 # Show the plot
 plt.show()
 
@@ -131,7 +135,7 @@ peak_hour_day = accident_hour_day.loc[accident_hour_day.groupby('AccidentWeekDay
 # Print the peak hour of accidents for each day of the week
 print(peak_hour_day)
 
-"""Plot the number of accidents over time."""
+"""Plot the number of accidents over time"""
 # Convert the 'AccidentYear' column to datetime format
 accidentDf['AccidentYear'] = pd.to_datetime(accidentDf['AccidentYear'], format='%Y')
 
@@ -141,14 +145,23 @@ accidents_per_year = accidentDf.groupby(accidentDf['AccidentYear'].dt.year).size
 # Create a new figure and set the figure size
 fig, ax = plt.subplots(figsize=(10, 6))
 
-# Plot the number of accidents over time
-ax.plot(accidents_per_year.index, accidents_per_year.values)
+# Plot the number of accidents over time using a line plot
+ax.plot(accidents_per_year.index, accidents_per_year.values, marker='o', linestyle='-', color='b',
+        label='Total Accidents')
+
+# Highlight the peak year with a red star marker
+peak_year = accidents_per_year.idxmax()
+ax.plot(peak_year, accidents_per_year.max(), marker='*', markersize=10, color='r', label=f'Peak Year: {peak_year}')
 
 # Set the title and labels
 ax.set_title('Number of Accidents Over Time')
 ax.set_xlabel('Year')
 ax.set_ylabel('Number of Accidents')
 
+# Add legend to the plot
+ax.legend()
+
+# plt.savefig("./plots/accidents_over_time.png")
 # Show the plot
 plt.show()
 
@@ -173,6 +186,43 @@ ax.set_ylabel('Number of Accidents')
 # Show the plot
 plt.show()
 
+"""I notice that 2018 was the peak year of accidents, let's have a closer look into it"""
+# Filter the data for the year 2018
+accidents_2018 = accidentDf[accidentDf['AccidentYear'].dt.year == 2018]
+
+# Group the data by 'AccidentMonth_en' and 'AccidentType_en' and count the number of accidents in each month for each type
+# accidents_per_month_type_2018 = accidents_2018.groupby(['AccidentMonth_en', 'AccidentType_en']).size().unstack().reindex(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
+accidents_per_month_type_2018 = accidents_2018.groupby(['AccidentMonth_en', 'AccidentType'],
+                                                       observed=True).size().unstack().reindex(
+    ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November',
+     'December'])
+
+# Create a new figure and set the figure size
+fig, ax = plt.subplots(figsize=(12, 8))
+
+# Define a color map for different types of accidents
+colors = plt.cm.get_cmap('tab10', len(accidents_per_month_type_2018.columns))
+# colors = plt.cm.get_cmap('tab10', len(accidents_per_month_type_2018.columns))
+
+# Plot the frequency of accidents for each month with different colors for each type
+accidents_per_month_type_2018.plot(kind='bar', stacked=True, ax=ax,
+                                   color=[colors(i) for i in range(len(accidents_per_month_type_2018.columns))])
+
+# Rotate the x-axis labels and make them fit into the figure
+fig.autofmt_xdate()
+
+# Set the title and labels
+ax.set_title('Monthly Distribution of Accidents in 2018 by Type')
+ax.set_xlabel('Month')
+ax.set_ylabel('Number of Accidents')
+
+# Add legend to the plot
+ax.legend(title='Accident Type', bbox_to_anchor=(1, 1))
+
+# plt.savefig("./plots/2018.png")
+# Show the plot
+plt.show()
+
 # Identify and exclude non-numeric columns
 numeric_columns = accidentDf.select_dtypes(include=['number']).columns
 correlation_matrix = accidentDf[numeric_columns].corr()
@@ -182,7 +232,8 @@ print(correlation_matrix)
 
 """Identify patterns in accident types using categorical data analysis"""
 # Create a pivot table to analyze accident types over the years
-accident_types_over_time = accidentDf.pivot_table(index='AccidentYear', columns='AccidentType_en', aggfunc='size', fill_value=0)
+accident_types_over_time = accidentDf.pivot_table(index='AccidentYear', columns='AccidentType_en', aggfunc='size',
+                                                  fill_value=0)
 
 # Create a new figure and set the figure size
 fig, ax = plt.subplots(figsize=(12, 8))
@@ -195,32 +246,15 @@ ax.set_title('Accident Types Over Time')
 ax.set_xlabel('Year')
 ax.set_ylabel('Frequency')
 
+# plt.savefig("./plots/patterns.png")
 # Show the plot
 plt.legend(title='Accident Type', loc='upper left')
 plt.show()
 
-
 """Accident Types by Severity"""
-# Create a dictionary for accident types
-"""accident_types = {
-    "at0": "Skidding/self-accident",
-    "at1": "Overtaking/changing lanes",
-    "at2": "Rear-end collision",
-    "at3": "Turning left or right",
-    "at4": "Turning-into main road",
-    "at5": "Crossing the lane",
-    "at6": "Head-on collision",
-    "at7": "Parking",
-    "at8": "Pedestrians",
-    "at9": "Animals",
-    "at00": "Other"
-}
-
-# Replace the codes in the 'AccidentType' column with their descriptions
-accidentDf['AccidentType_en'] = accidentDf['AccidentType'].map(accident_types)
-"""
 # Create a pivot table to analyze accident types based on severity
-accident_types_by_severity = accidentDf.pivot_table(index='AccidentType_en', columns='AccidentSeverityCategory_en', aggfunc='size', fill_value=0)
+accident_types_by_severity = accidentDf.pivot_table(index='AccidentType_en', columns='AccidentSeverityCategory_en',
+                                                    aggfunc='size', fill_value=0)
 
 # Create a new figure and set the figure size
 fig, ax = plt.subplots(figsize=(12, 8))
@@ -235,115 +269,73 @@ ax.set_title('Accident Types by Severity')
 ax.set_xlabel('Accident Type')
 ax.set_ylabel('Frequency')
 
+# plt.savefig("./plots/types_by_severity.png")
+
 # Show the plot
-plt.legend(title='Severity', loc='upper right')
+plt.legend(title='Severity', loc='upper right', bbox_to_anchor=(1.12, 1.1))
 plt.show()
+
+"""severity of accidents on each day of the week"""
+# Create a cross-tabulation of 'AccidentSeverityCategory' and 'AccidentWeekDay_en'
+crosstab = pd.crosstab(accidentDf['AccidentWeekDay_en'], accidentDf['AccidentSeverityCategory_en'])
+
+# Reorder the index to make the days of the week in order
+crosstab = crosstab.reindex(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
+
+# Create a new figure and set the figure size
+fig, ax = plt.subplots(figsize=(10, 6))
+
+# Plot a stacked bar chart for accident severity by day of the week
+crosstab.plot(kind='bar', stacked=True, ax=ax)
+
+# Rotate the x-axis labels and make them fit into the figure
+fig.autofmt_xdate()
+
+# Set the title and labels
+ax.set_title('Severity of Accidents on Each Day of the Week')
+ax.set_xlabel('Day of the Week')
+ax.set_ylabel('Number of Accidents')
+
+# Move the legend outside of the chart and a few more pixels to the right
+ax.legend(title='Severity', loc='upper right', bbox_to_anchor=(1.12, 1.1))
+
+# plt.savefig("./plots/severity.png")
+# Show the plot
+plt.show()
+
 
 """Accident Types Heatmap"""
-# # Create a cross-tabulation for accident types
-# accident_types_cross_tab = pd.crosstab(accidentDf['AccidentWeekDay_en'], accidentDf['AccidentType_en'])
-#
-# # Create a new figure and set the figure size
-# fig, ax = plt.subplots(figsize=(14, 10))
-#
-# # Plot a heatmap for the correlation between accident types
-# plt.pcolor(accident_types_cross_tab, cmap='Blues', edgecolors='k', linewidths=0.5)
-#
-# # Set the title and labels
-# ax.set_title('Accident Types Heatmap')
-# ax.set_xlabel('Accident Type')
-# ax.set_ylabel('Day of the Week')
-#
-# # Show the plot
-# plt.colorbar(label='Frequency')
-# plt.show()
+# Create a cross-tabulation for accident types
+accident_types_cross_tab = pd.crosstab(accidentDf['AccidentWeekDay_en'], accidentDf['AccidentType_en'])
 
+# Reorder the index to have weekdays in the correct order
+weekday_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+accident_types_cross_tab = accident_types_cross_tab.reindex(weekday_order)
 
-""" Bar Plot for Accident Types with Labels"""
+# Calculate the total number of accidents for each accident type
+total_accidents_by_type = accident_types_cross_tab.sum(axis=0)
+
+# Sort the accident types based on the total number of accidents in descending order
+sorted_accident_types = total_accidents_by_type.sort_values(ascending=False).index
+accident_types_cross_tab = accident_types_cross_tab[sorted_accident_types]
+
 # Create a new figure and set the figure size
-fig, ax = plt.subplots(figsize=(10, 6))
+fig, ax = plt.subplots(figsize=(14, 10))
 
-# Plot the frequency of different types of accidents with labels
-bars = ax.bar(accident_type_freq.index, accident_type_freq.values)
+# Plot a heatmap for the correlation between accident types using seaborn
+sns.heatmap(accident_types_cross_tab, cmap='Blues', annot=True, fmt='d', linewidths=0.5, ax=ax)
 
-# Add labels to the bars
-for bar in bars:
-    yval = bar.get_height()
-    plt.text(bar.get_x() + bar.get_width()/2, yval + 20, round(yval, 1), ha='center', va='bottom')
+# Rotate the labels of accident types by 45 degrees
+plt.xticks(rotation=45, ha='right')
 
-# Rotate the x-axis labels and make them fit into the figure
-fig.autofmt_xdate()
+# Rotate the labels of weekdays horizontally
+plt.yticks(rotation=0)
 
 # Set the title and labels
-ax.set_title('Frequency of Different Types of Accidents')
+ax.set_title('Accident Types Heatmap')
 ax.set_xlabel('Accident Type')
-ax.set_ylabel('Frequency')
+ax.set_ylabel('Day of the Week')
 
+# plt.savefig("./plots/heatmap.png")
 # Show the plot
 plt.show()
-
-""" Line Plot for Temporal Trends"""
-# Convert the 'AccidentYear' column to datetime format
-accidentDf['AccidentYear'] = pd.to_datetime(accidentDf['AccidentYear'], format='%Y')
-
-# Group the data by 'AccidentYear' and count the number of accidents in each year
-accidents_per_year = accidentDf.groupby(accidentDf['AccidentYear'].dt.year).size()
-
-# Create a new figure and set the figure size
-fig, ax = plt.subplots(figsize=(10, 6))
-
-# Plot the number of accidents over time using a line plot
-ax.plot(accidents_per_year.index, accidents_per_year.values, marker='o', linestyle='-', color='b', label='Total Accidents')
-
-# Highlight the peak year with a red star marker
-peak_year = accidents_per_year.idxmax()
-ax.plot(peak_year, accidents_per_year.max(), marker='*', markersize=10, color='r', label=f'Peak Year: {peak_year}')
-
-# Set the title and labels
-ax.set_title('Number of Accidents Over Time')
-ax.set_xlabel('Year')
-ax.set_ylabel('Number of Accidents')
-
-# Add legend to the plot
-ax.legend()
-
-# Show the plot
-plt.show()
-
-"""2018"""
-# Filter the data for the year 2018
-accidents_2018 = accidentDf[accidentDf['AccidentYear'].dt.year == 2018]
-
-# Group the data by 'AccidentMonth_en' and 'AccidentType_en' and count the number of accidents in each month for each type
-# accidents_per_month_type_2018 = accidents_2018.groupby(['AccidentMonth_en', 'AccidentType_en']).size().unstack().reindex(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
-accidents_per_month_type_2018 = accidents_2018.groupby(['AccidentMonth_en', 'AccidentType_en'], observed=True).size().unstack().reindex(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
-
-# Create a new figure and set the figure size
-fig, ax = plt.subplots(figsize=(12, 8))
-
-# Define a color map for different types of accidents
-colors = plt.cm.get_cmap('tab10', len(accidents_per_month_type_2018.columns))
-# colors = plt.cm.get_cmap('tab10', len(accidents_per_month_type_2018.columns))
-
-# Plot the frequency of accidents for each month with different colors for each type
-accidents_per_month_type_2018.plot(kind='bar', stacked=True, ax=ax, color=[colors(i) for i in range(len(accidents_per_month_type_2018.columns))])
-
-# Rotate the x-axis labels and make them fit into the figure
-fig.autofmt_xdate()
-
-# Set the title and labels
-ax.set_title('Monthly Distribution of Accidents in 2018 by Type')
-ax.set_xlabel('Month')
-ax.set_ylabel('Number of Accidents')
-
-# Add legend to the plot
-ax.legend(title='Accident Type', bbox_to_anchor=(1, 1))
-
-# Show the plot
-plt.show()
-
-
-
-
-
-
